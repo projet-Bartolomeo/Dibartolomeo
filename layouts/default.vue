1d10a18de6be09003a5e40c0ae51ab224a4d3fd2
MatthiasChometon <matthias.chometon@ynov.com>
2021-10-24 12:38:02 +0100

    feat: initialize app

---
+++ layouts/default.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app dark>
+    <SideBar />
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-navigation-drawer
+      v-model="rightDrawer"
+      :right="right"
+      temporary
+      fixed
+    >
+      <v-list>
+        <v-list-item @click.native="right = !right">
+          <v-list-item-action>
+            <v-icon light>
+              mdi-repeat
+            </v-icon>
+          </v-list-item-action>
+          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      right: true,
+      rightDrawer: false,
+    }
+  },
+}
+</script>
