c26605dc978466dc723c59b38ac4d8eb50d5e31a
johannadznd <johanna.dezarnaud@ynov.com>
2021-10-29 17:29:53 +0200

    feat: add new page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,30 +1,16 @@
 <template>
-  <v-app light>
-    <SideBar />
-    <v-main>
-      <v-container>
-        <Nuxt />
-      </v-container>
-    </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-  </v-app>
+  <v-app>
+  <v-navigation-drawer app>
+    <side-bar />
+  </v-navigation-drawer>
+  <v-main>
+    <v-container fluid>
+      <router-view></router-view>
+    </v-container>
+  </v-main>
+</v-app>
 </template>
+
 
 <script>
 
