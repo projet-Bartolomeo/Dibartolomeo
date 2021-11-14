a889ca1e37e53831f8e04a11c42f23303c3b4c09
MatthiasChometon <matthias.chometon@ynov.com>
2021-11-14 18:12:34 +0100

    style: change alignement

--- nuxt.config.js
+++ nuxt.config.js
@@ -89,14 +89,13 @@ export default {
       light: true,
       themes: {
         dark: {
-            primary: '#1976D2',
-            secondary: '#424242',
-            accent: '#82B1FF',
-            error: '#FF5252',
-            info: '#2196F3',
-            success: '#4CAF50',
-            warning: '#FFC107',
-          
+          primary: '#1976D2',
+          secondary: '#424242',
+          accent: '#82B1FF',
+          error: '#FF5252',
+          info: '#2196F3',
+          success: '#4CAF50',
+          warning: '#FFC107'
         }
       }
     }
