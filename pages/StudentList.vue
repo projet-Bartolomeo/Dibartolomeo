ca00cff31ae293b401815b1125100ec551d89724
juva-quentin <71253269+juva-quentin@users.noreply.github.com>
2021-11-17 14:32:18 +0100

    Revert "Merge pull request #29 from projet-Bartolomeo/feature/StudentList"
    This reverts commit 06dbfbe0c7f210af57ec51516188e05ec1a6400b, reversing
    changes made to bf00d8d1059bd2a3b229d4dfcfc22b19d160c522.

--- pages/StudentList.vue
+++ pages/StudentList.vue
@@ -1,35 +1,5 @@
 <template>
-<div>
-<v-row class="ma-4 justify-space-between">
-    <h1>Listes des élèves</h1>
-    <v-btn color="teal lighten-2" style="color:white">
-                Ajouter un élève
-          
-              </v-btn>
-  </v-row>
-    <div>
-        <v-app id="inspire">
-            <v-row class="ma-4 justify-space-around">
-            <v-col cols="12" sm="4" md="4">
-                        <v-btn 
-                        color="teal lighten-2" style="color:white"
-                depressed
-                disabled>
-              Envoyer un message
-                </v-btn>
-            </v-col>
-           <v-col cols="12" sm="4" md="4">
-                        <v-text-field
-            
-            label="Recherche"
-            outlined>
-            </v-text-field>
-            </v-col>
-            </v-row>
-            
-        </v-app>
-    </div>
-    </div>
+    <h1>test</h1>
 </template>
 
 <script>
--- pages/test.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <v-row class="justify-end">
-      <div>test</div>
-      <div>test</div>
-    </v-row> 
-  </div>
-</template>
