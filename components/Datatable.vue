847cd766ce36c432b24aa283e34e1fd184303fb4
MatthiasChometon <matthiaschometon787@gmail.com>
2021-11-23 19:17:21 +0100

    feat: add props to datatable

--- components/Datatable.vue
+++ components/Datatable.vue
@@ -1,83 +1,159 @@
 <template>
-  <v-data-table
-    :headers="headers"
-    :items="datas"
-    sort-by="calories"
-    class="elevation-1"
-  >
-    <template v-slot:top>
-      <v-toolbar flat>
-        <v-toolbar-title>My {{ type }} </v-toolbar-title>
-        <v-divider class="mx-4" inset vertical></v-divider>
-        <v-spacer></v-spacer>
-        <v-dialog v-model="dialog" max-width="500px">
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">
-              New Item
-            </v-btn>
-          </template>
-          <v-card>
-            <v-card-title>
-              <span class="text-h5">{{ formTitle }}</span>
-            </v-card-title>
-
-            <v-card-text>
-              <v-container>
-                <v-row>
-                  <v-col
-                    v-for="item in Object.entries(editedItem)"
-                    :key="item[1]"
-                    cols="12"
-                    sm="6"
-                    md="4"
-                  >
-                    <v-text-field
-                      v-model="editedItem[item[0]]"
-                      :label="item[0]"
-                    ></v-text-field>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-card-text>
-
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="close"> Cancel </v-btn>
-              <v-btn color="blue darken-1" text @click="save"> Save </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-        <v-dialog v-model="dialogDelete" max-width="500px">
-          <v-card>
-            <v-card-title class="text-h5"
-              >Are you sure you want to delete this item?</v-card-title
-            >
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="closeDelete"
-                >Cancel</v-btn
+  <v-card>
+    <v-card-title v-if="actions.includes('search')">
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Rechercher"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table
+      :headers="filteredHeaders"
+      :items="ressource"
+      sort-by="calories"
+      class="elevation-1"
+      :footer-props="{
+        'items-per-page-text': `${type}s par page`,
+      }"
+      :search="search"
+      v-model="selected"
+      :single-select="singleSelect"
+      item-key="id"
+      :show-select="showSelect"
+    >
+      <template v-slot:top>
+        <v-toolbar flat>
+          <v-toolbar-title>Mes {{ type }}s </v-toolbar-title>
+          <v-divider class="mx-4" inset vertical></v-divider>
+          <v-spacer></v-spacer>
+          <v-dialog v-model="dialog" max-width="500px">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                v-if="actions.includes('create')"
+                elevation="7"
+                color="green"
+                class="mb-2 white--text"
+                v-bind="attrs"
+                v-on="on"
               >
-              <v-btn color="blue darken-1" text @click="deleteItemConfirm"
-                >OK</v-btn
+                Créer un {{ type }}
+              </v-btn>
+              <v-btn
+                v-if="actions.includes('message')"
+                :disabled="selected.length === 0"
+                @click="sendMessage"
+                elevation="7"
+                color="green"
+                class="mb-2 mr-4 white--text"
               >
-              <v-spacer></v-spacer>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-      </v-toolbar>
-    </template>
-    <template v-slot:[`item.actions`]="{ item }">
-      <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
-      <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
-    </template>
-    <template v-slot:no-data>
-      <v-btn color="primary" @click="initialize"> Reset </v-btn>
-    </template>
-  </v-data-table>
+                Envoyer un message
+              </v-btn>
+            </template>
+            <v-card>
+              <v-card-title>
+                <span class="text-h5">{{ formTitle }}</span>
+              </v-card-title>
+
+              <v-card-text>
+                <v-container>
+                  <v-row>
+                    <v-col
+                      v-for="item in Object.entries(editedItemFiltered)"
+                      :key="item[1]"
+                      cols="12"
+                      sm="6"
+                      md="4"
+                    >
+                      <v-text-field
+                        v-if="currentHeader[item[0]].type === 'input'"
+                        v-model="editedItem[item[0]]"
+                        :label="currentHeader[item[0]].text"
+                      ></v-text-field>
+                      <v-switch
+                        v-if="currentHeader[item[0]].type === 'switch'"
+                        v-model="editedItem[item[0]]"
+                        inset
+                        :label="currentHeader[item[0]].text"
+                      ></v-switch>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="close">
+                  Annuler
+                </v-btn>
+                <v-btn color="blue darken-1" text @click="save">
+                  Sauvegarder
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+          <v-dialog v-model="dialogDelete" max-width="500px">
+            <v-card>
+              <v-card-title class="text-h5 overflow-wrap-normal"
+                >Etes-vous sur de vouloir supprimer cet item ?</v-card-title
+              >
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="closeDelete"
+                  >Annuler</v-btn
+                >
+                <v-btn color="blue darken-1" text @click="deleteItemConfirm"
+                  >Supprimer</v-btn
+                >
+                <v-btn
+                  v-if="actions.includes('deleteLesson')"
+                  color="blue darken-1"
+                  text
+                  @click="deleteItemConfirm"
+                  >Supprimer tous</v-btn
+                >
+                <v-spacer></v-spacer>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-toolbar>
+      </template>
+      <template v-if="actions !== undefined" v-slot:[`item.actions`]="{ item }">
+        <v-icon
+          v-if="actions.includes('message')"
+          @click="sendMessage(item)"
+          small
+          class="mr-1"
+        >
+          mdi-message
+        </v-icon>
+        <v-icon
+          v-if="actions.includes('edit')"
+          small
+          class="mr-1"
+          @click="editItem(item)"
+        >
+          mdi-pencil
+        </v-icon>
+        <v-icon
+          v-if="actions.includes('delete')"
+          small
+          class="mr-1"
+          @click="deleteItem(item)"
+        >
+          mdi-delete
+        </v-icon>
+        <slot v-bind:item="item"></slot>
+      </template>
+      <template v-slot:no-data>
+        <v-btn color="primary" @click="initialize"> Réinitialiser </v-btn>
+      </template>
+    </v-data-table>
+  </v-card>
 </template>
 
 <script>
-
 export default {
   props: {
     type: {
@@ -92,31 +168,44 @@ export default {
       type: Array,
       required: true,
     },
+    actions: {
+      type: Array,
+      required: false,
+    },
   },
-  data: () => ({
+  data () {
+    return {
+    showSelect: false,
+    search: '',
     dialog: false,
     dialogDelete: false,
-    desserts: [],
+    ressource: [],
     editedIndex: -1,
-    editedItem: {
-      name: '',
-      calories: 0,
-      fat: 0,
-      carbs: 0,
-      protein: 0,
-    },
-    defaultItem: {
-      name: '',
-      calories: 0,
-      fat: 0,
-      carbs: 0,
-      protein: 0,
-    },
-  }),
-
+    editedItem: {},
+    defaultItem: {},
+    currentHeader: {},
+    singleSelect: false,
+    selected: [],
+    }
+  },
   computed: {
     formTitle() {
-      return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+      return this.editedIndex === -1
+        ? `Créer un ${this.type}`
+        : `Modifier un ${this.type}`
+    },
+    editedItemFiltered() {
+      delete this.editedItem.id
+      return this.editedItem
+    },
+    filteredHeaders() {
+      return this.headers.reduce((newHeaders, currentHeader) => {
+        if (currentHeader.value === 'actions' && this.actions === undefined) {
+          return newHeaders
+        }
+        newHeaders.push(currentHeader)
+        return newHeaders
+      }, [])
     },
   },
 
@@ -129,23 +218,40 @@ export default {
     },
   },
 
-  created() {},
+  created() {
+    if (this.$props.actions.includes('message')) this.showSelect = true
+    this.defaultItem = this.editedItem = this.headers.reduce(
+      (defaultItem, currentHeader) => {
+        if (currentHeader.initialValue === undefined) return defaultItem
+        defaultItem[currentHeader.value] = currentHeader.initialValue
+        return defaultItem
+      },
+      {}
+    )
+    this.ressource = this.$props.datas
+    this.currentHeader = this.headers.reduce((newHeader, currentHeader) => {
+      newHeader[currentHeader.value] = currentHeader
+      return newHeader
+    }, {})
+  },
 
   methods: {
+    sendMessage() {},
+
     editItem(item) {
-      this.editedIndex = this.desserts.indexOf(item)
+      this.editedIndex = this.ressource.indexOf(item)
       this.editedItem = Object.assign({}, item)
       this.dialog = true
     },
 
     deleteItem(item) {
-      this.editedIndex = this.desserts.indexOf(item)
+      this.editedIndex = this.ressource.indexOf(item)
       this.editedItem = Object.assign({}, item)
       this.dialogDelete = true
     },
 
     deleteItemConfirm() {
-      this.desserts.splice(this.editedIndex, 1)
+      this.ressource.splice(this.editedIndex, 1)
       this.closeDelete()
     },
 
@@ -167,12 +273,23 @@ export default {
 
     save() {
       if (this.editedIndex > -1) {
-        Object.assign(this.desserts[this.editedIndex], this.editedItem)
+        Object.assign(this.ressource[this.editedIndex], this.editedItem)
       } else {
-        this.desserts.push(this.editedItem)
+        this.ressource.push(this.editedItem)
       }
       this.close()
     },
   },
 }
 </script>
+
+
+<style>
+.overflow-wrap-normal {
+  word-break: break-word;
+  text-align: center;
+}
+.nuxtlink {
+  text-decoration: none;
+}
+</style>
