a35bd5e337738edb0ef7efff8d626aef5febf115
MattisBe <84279749+MattisBe@users.noreply.github.com>
2021-11-27 13:12:30 +0100

    feat: add getlessonbyid action

--- store/lesson.js
+++ store/lesson.js
@@ -8,5 +8,9 @@ export const actions = {
     async createLesson({ commit }, newLesson) {
         const lesson = await this.$fire.firestore.collection('lesson').add(newLesson)
         return lesson
-    }
+    },
+    async getLessonById({ commit }, id) {
+        const lesson = await this.$fire.firestore.collection('lesson').doc(id).get()
+        return lesson.data().lesson
+    } 
 }
