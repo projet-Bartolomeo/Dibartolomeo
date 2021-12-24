215889ab91a594cf06d8c6b4f12003dc7fb46a57
Ruizlouka <ruizlouka981@gmail.com>
2021-12-24 13:30:20 +0100

    feat: correct

---
+++ services/DateHelper.js
@@ -0,0 +1,33 @@
+export function convertTimestampToDate(date) {
+    const timestamp = date * 1000
+  
+
+    let convertedDate = new Date(timestamp)
+    
+
+    let eh = convertedDate.getHours()
+    if (eh < 10) {
+      eh = '0' + eh
+    }
+    let em = convertedDate.getMinutes()
+    if (em < 10) {
+      em = '0' + em
+    }
+
+    
+
+    convertedDate =
+      convertedDate.getDate() +
+      '/' +
+      (convertedDate.getMonth() + 1) +
+      '/' +
+      convertedDate.getFullYear() +
+      ' ' +
+      eh +
+      ':' +
+      em
+
+   
+        return convertedDate
+    
+}
