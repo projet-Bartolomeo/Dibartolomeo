bf8a4f8d5d8249ba9b18e4f6ddff7e66026a0d1e
johannadznd <johanna.dezarnaud@ynov.com>
2021-12-25 20:17:05 +0100

    feat: add textField Components

---
+++ components/TextField.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <v-text-field v-model="input"></v-text-field>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    get: {
+      required: true,
+    },
+    fieldName: {
+      required: true,
+    },
+    commit: {
+      type: String,
+      required: true,
+    },
+    stateName: {
+      type: String,
+      required: true,
+    },
+    number: {
+      type: Boolean,
+      required: false,
+    },
+  },
+  computed: {
+    input: {
+      get() {
+        return this.$props.get
+      },
+      set(newValue) {
+        this.$store.commit(this.$props.commit, {
+          payload: { [this.$props.fieldName]: newValue },
+          stateName: this.$props.stateName,
+        })
+      },
+    },
+  },
+}
+</script>
