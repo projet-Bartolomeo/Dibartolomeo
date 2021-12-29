8ddbdadc112912cc821e530f5f81ea03d0ab8067
MatthiasChometon <matthiaschometon787@gmail.com>
2021-12-29 09:59:03 +0100

    feat: add multiples converter

--- services/dateHelper.js
+++ services/dateHelper.js
@@ -1,27 +1,59 @@
-export function convertTimestampToDate(date) {
-    const timestamp = date.seconds * 1000
 
-    let convertedDate = new Date(timestamp)
-
-    let eh = convertedDate.getHours()
-    if (eh < 10) {
-        eh = '0' + eh
-    }
-    let em = convertedDate.getMinutes()
+function getMinutes(date) {
+    let em = date.getMinutes()
     if (em < 10) {
         em = '0' + em
     }
+    return em
+}
 
-    convertedDate =
-        convertedDate.getFullYear() +
+function getHours(date) {
+    let eh = date.getHours()
+    if (eh < 10) {
+        eh = '0' + eh
+    }
+    return eh
+}
+
+export function convertTimestampToDate(timestamp) {
+    return new Date(timestamp.seconds * 1000)
+}
+
+export function convertTimestampToReadableDate(timestamp) {
+    let timestampConverted = timestamp.seconds * 1000
+    timestampConverted = new Date(timestampConverted)
+
+    const date =
+        timestampConverted.getFullYear() +
         '-' +
-        (convertedDate.getMonth() + 1) +
+        (timestampConverted.getMonth() + 1) +
         '-' +
-        convertedDate.getDate() +
+        timestampConverted.getDate() +
         ' ' +
-        eh +
+        getMinutes(timestampConverted) +
         ':' +
-        em
+        getHours(timestampConverted)
 
-    return convertedDate
+    return date
+}
+
+export function convertDateToIso(date) {
+    return `${date.getFullYear()}-${(date.getMonth() + 1)}-${date.getDate()}`
+}
+
+export function getHoursAndMinutes(date) {
+    return `${getHours(date)}:${getMinutes(date)}`
+}
+
+export function convertStringToDate(date, hourly) {
+    const newDate = new Date(date)
+    const convertedDate = newDate.getFullYear() +
+        '-' +
+        (newDate.getMonth() + 1) +
+        '-' +
+        newDate.getDate() +
+        ' ' +
+        hourly
+
+    return new Date(convertedDate)
 }
