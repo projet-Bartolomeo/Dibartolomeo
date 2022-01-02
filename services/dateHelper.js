11ff4d1f06fd39e8bf29ff4d0e05a2c4f805e1fb
MatthiasChometon <matthiaschometon787@gmail.com>
2022-01-02 12:03:51 +0100

    feat: add getDay and getMonth functions

--- services/dateHelper.js
+++ services/dateHelper.js
@@ -1,18 +1,26 @@
 
 function getMinutes(date) {
     let em = date.getMinutes()
-    if (em < 10) {
-        em = '0' + em
-    }
+    if (em < 10) em = '0' + em
     return em
 }
 
 function getHours(date) {
     let eh = date.getHours()
-    if (eh < 10) {
-        eh = '0' + eh
-    }
+    if (eh < 10) eh = '0' + eh
     return eh
+}
+
+function getDay(date) {
+    let day = date.getDate()
+    if (day < 10) day = `0${day}`
+    return day
+}
+
+function getMonth(date) {
+    let month = date.getMonth() + 1
+    if (month < 10) month = `0${month}`
+    return month
 }
 
 export function convertTimestampToDate(timestamp) {
@@ -23,22 +31,17 @@ export function convertTimestampToReadableDate(timestamp) {
     let timestampConverted = timestamp.seconds * 1000
     timestampConverted = new Date(timestampConverted)
 
-    const date =
-        timestampConverted.getFullYear() +
-        '-' +
-        (timestampConverted.getMonth() + 1) +
-        '-' +
-        timestampConverted.getDate() +
-        ' ' +
-        getMinutes(timestampConverted) +
-        ':' +
-        getHours(timestampConverted)
+    const year = timestampConverted.getFullYear()
+    const month = getMonth(timestampConverted)
+    const day = getDay(timestampConverted)
+    const minutes = getMinutes(timestampConverted)
+    const hours = getHours(timestampConverted)
 
-    return date
+    return `${year}-${month}-${day} ${minutes}:${hours}`
 }
 
 export function convertDateToIso(date) {
-    return `${date.getFullYear()}-${(date.getMonth() + 1)}-${date.getDate()}`
+    return `${date.getFullYear()}-${getMonth(date)}-${getDay(date)}`
 }
 
 export function getHoursAndMinutes(date) {
@@ -47,13 +50,11 @@ export function getHoursAndMinutes(date) {
 
 export function convertStringToDate(date, hourly) {
     const newDate = new Date(date)
-    const convertedDate = newDate.getFullYear() +
-        '-' +
-        (newDate.getMonth() + 1) +
-        '-' +
-        newDate.getDate() +
-        ' ' +
-        hourly
+
+    const month = getMonth(newDate)
+    const day = getDay(newDate)
+
+    const convertedDate = `${newDate.getFullYear()}-${month}-${day} hourly`
 
     return new Date(convertedDate)
 }
