19f5f34400c48f9c26f596f7cd464c4a3d528f49
MatthiasChometon <matthiaschometon787@gmail.com>
2022-01-06 13:32:22 +0100

    feat: delete useless message

--- store/message.js
+++ store/message.js
@@ -21,9 +21,7 @@ export const actions = {
                 ? 'Dibartoloméo - création du compte'
                 : 'Dibartoloméo - vous avez recu un message de votre professeur'
 
-            const content = `<p>Dibartoloméo<p><a>${contentMessage}</a></h3><br /> <img class="fit-picture"
-            src="https://trello.com/1/cards/6160076387267344d2d71b77/attachments/61806466e5b595625772b640/download/Capture_d%E2%80%99%C3%A9cran_2021-11-01_%C3%A0_23.03.58.png"
-            alt="https://trello.com/1/cards/6160076387267344d2d71b77/attachments/61806466e5b595625772b640/download/Capture_d%E2%80%99%C3%A9cran_2021-11-01_%C3%A0_23.03.58.png">`
+            const content = `<a>${contentMessage}</a>`
 
             await this.$axios.post('https://mailer-dibartolomeo.herokuapp.com/email',
                 {
