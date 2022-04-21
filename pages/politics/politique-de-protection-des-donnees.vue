06e3e49c2e4c9492957a95d61853858742291a81
RFENZL <romain.fenzl@ynov.com>
2022-04-21 19:42:02 +0200

    feat: add ppd

---
+++ pages/politics/politique-de-protection-des-donnees.vue
@@ -0,0 +1,119 @@
+<template>
+<div>
+Politique de protection des données
+
+Yoko Gang accorde une grande importance au respect de votre vie privée et nous avons à cœur de construire avec vous des relations fortes et durables. La protection de vos données personnelles est primordiale à nos yeux.
+
+C’est pourquoi, nous mettons à votre disposition notre Politique de protection des données afin de vous informer, de la façon la plus claire possible, des traitements de données personnelles que nous mettons en œuvre.
+ 1. Qui sommes-nous et que recouvre la présente Politique ?
+
+La présente Politique de protection des données s’applique aux données personnelles que nous collectons lorsque vous utilisez notre site internet Dibartolomeoshop.fr (ci-après dénommé le « Site »).
+
+Nous expliquerons, ci-dessous, de quelle manière nous collectons, utilisons et partageons vos données personnelles, ainsi que les choix qui s’offrent à vous au sujet de vos données personnelles.
+2. Qui est le responsable du traitement de vos données personnelles ?
+
+Yoko Gang, dont le siège social est situé au 75, rue de Lourmel – 75 015 Paris, est le responsable des traitements mis en œuvre sur son Site.
+3. Quelles sont les données personnelles que nous collectons lorsque vous utilisez notre Site ?
+
+Nous recueillons vos données personnelles lorsque vous nous les communiquez directement ou lorsque vous utilisez notre Site.
+3.1. Les données personnelles que vous nous fournissez
+
+Nous collectons vos données personnelles à l’occasion des différents contacts que vous pouvez avoir avec nous. Nous les collectons notamment lors de la création de votre compte sur notre Site ou lorsque vous passez commande.
+
+Les données personnelles que nous collectons sont les suivantes :
+
+    Informations relatives à votre compte : nous avons besoin d’informations basiques pour créer votre compte client. Nous collectons votre nom, prénom, adresse mail et votre mot de passe ;
+    Données personnelles relatives aux transactions et à la facturation : si vous passez commande, nous collecterons votre nom, prénom, adresse de livraison et de facturation ainsi que vos coordonnées bancaires pour traiter votre paiement et finaliser la transaction ;
+    Communications avec nous : vous pouvez également nous fournir des informations lorsque vous contactez notre service client, comme votre nom, adresse email et numéro de commande ;
+    Newsletter : si vous choisissez de vous inscrire à notre newsletter, nous collecterons votre adresse mail.
+
+ 3.2. Les données personnelles que nous collectons automatiquement
+
+Nous collectons également certaines informations de manière automatique lorsque vous utilisez notre Site :
+
+    Données personnelles relatives à vos connexions : nous collectons toutes les données personnelles que les navigateurs, les appareils mobiles et les serveurs nous transmettent, comme le type de navigateur, l’adresse IP, les identifiants d’appareil unique, les choix de langue, la date et l’heure d’accès, le système d’exploitation et les informations de réseau mobile. Nous collectons ces informations de connexion lorsque vous utilisez nos Services ;
+    Informations provenant des cookies et d’autres technologies : un cookie est un ensemble d’informations qu’un site enregistre sur l’ordinateur d’un visiteur et que le navigateur de ce visiteur fournit au site chaque fois que le visiteur revient. Les pixels invisibles sont de petits blocs de codes présents sur les sites et les e-mails. Yoko Gang se sert de cookies et d’autres technologies, comme les pixels invisibles, pour nous aider à identifier et garder trace des utilisateurs, comprendre l’utilisation qui est faite de notre Site, suivre et comprendre l’efficacité d’une campagne d’e-mailing, ainsi que pour diffuser des publicités ciblées. Pour en savoir plus sur l’utilisation que l’on fait des cookies et autres technologies de suivi, veuillez consulter notre Politique relative aux cookies.
+
+ 4. Quelles sont les finalités pour lesquelles nous collectons vos données personnelles ?
+
+Yoko Gang s’assure de traiter vos données personnelles sur un fondement adéquat et pour des finalités déterminées.
+ 4.1. Fondements sur lesquels nous collectons et traitons vos données
+
+Yoko Gang s’assure de traiter vos données personnelles sur une base légale pertinente.
+
+Ainsi, les traitements que nous mettons en œuvre reposent sur :
+
+    L’exécution d’un engagement contractuel, par exemple : la création de votre compte client et le traitement de vos données de facturation lorsque vous passez commande ;
+    Le respect d’une obligation légale qui nous incombe ;
+    L’existence d’un intérêt légitime nous poussant à traiter vos données personnelles, comme pour améliorer notre Site afin de pouvoir vous proposer une meilleure expérience utilisateur, assurer le bon fonctionnement de notre Site et répondre à vos demandes lorsque vous utilisez le formulaire de contact présent sur le Site ;
+    Votre consentement, par exemple : avant de déposer et lire les cookies et autres traceurs, nous veillons à recueillir votre consentement. 
+
+4.2. Finalités pour lesquelles nous utilisons vos données personnelles 
+
+Nous utilisons vos données personnelles dans les conditions et aux fins énumérées ci-après :
+
+    Pour vous permettre d’utiliser pleinement notre Site et commander nos produits, par exemple : configurer et tenir votre compte, suivre vos commandes, consulter l’historique de vos commandes, ajouter et modifier vos adresses de livraisons et de facturations ;
+    Surveiller et analyser les tendances, et mieux comprendre la manière dont vous interagissez avec notre Site, ce qui nous aidera à l’améliorer en retour et à le rendre plus facile à utiliser ;
+    Mesurer, jauger et améliorer l’efficacité de notre publicité, et ainsi mieux comprendre les mécanismes de fidélisation de nos clients, par exemple : nous pouvons effectuer des analyses sur le nombre d’individus qui ont commandé un produit après avoir reçu un message marketing ;
+    Surveiller et empêcher l’apparition de problèmes sur notre Site ;
+    Vous informer, via e-mail par exemple, des offres et des promotions proposées par Yoko Gang et susceptibles de vous intéresser, solliciter votre avis ou vous tenir au courant de l’actualité de nos produits (dont vous pouvez vous désabonner à tout moment) ; 
+    Personnaliser votre expérience d’utilisation sur notre Site, cibler nos messages marketing à destination de certains groupes de clients (par exemple, ceux qui n’ont pas commandé depuis un certain laps de temps) et diffuser des publicités pertinentes.
+
+5. Qui peut avoir accès à vos données personnelles ?
+
+Yoko Gang partage vos données personnelles dans des circonstances précises et limitées. Nous avons mis en place des garanties appropriées pour préserver votre vie privée.
+
+Nous partageons vos données personnelles avec les acteurs suivants et/ou pour les finalités suivantes :
+
+    Prestataires externes : nous pouvons partager vos données personnelles avec nos prestataires pour vous fournir nos Services ;
+    Partenaires commerciaux : nous pouvons également partager vos données personnelles avec nos partenaires commerciaux ;
+    Les autorités de police, les autorités judiciaires ou administratives : nous pouvons divulguer vos données personnelles lorsqu’une obligation légale nous y oblige, comme en cas de réquisition judiciaire ;
+    Protection de nos droits : nous pouvons également divulguer vos données personnelles pour défendre ou faire valoir nos droits en justice.
+
+6. Pendant combien de temps conservons-nous vos données personnelles ?
+
+Nous supprimons vos données personnelles lorsqu’elles ne sont plus nécessaires aux fins pour lesquelles nous les avons collectées ; sous réserve qu’aucune obligation légale ou réglementaire nous oblige à les conserver.
+
+Par exemple, nous conservons vos données de connexion pendant 12 mois à compter de leur collecte, conformément à nos obligations légales.
+7. Comment protégeons-nous vos données personnelles ?
+
+Yoko Gang prend diverses mesures pour assurer la sécurité de vos données personnelles contre leur perte, leur utilisation abusive, leur accès non autorisé, leur divulgation, leur modification ou leur destruction.
+
+En particulier :
+
+    Dès la réception de vos données personnelles, nous appliquons des procédures et des mesures de sécurité strictes afin de nous efforcer d’empêcher tout accès non autorisé, l’accès aux bases de données personnelles étant strictement réservé aux personnes habilitées à en prendre connaissance dans le cadre de leurs missions ;
+    Nous exigeons de nos sous-traitants qu’ils s’engagent à respecter la sécurité et la confidentialité de vos données personnelles, et ce par voie contractuelle.
+
+8. Vos données personnelles sont-elles transférées en dehors de l’Union européenne ?
+
+En principe, nous conservons vos données personnelles au sein de l’Union européenne. Cependant, nous pouvons être amenés à transférer vos données personnelles à certains de nos sous-traitants ou partenaires se situant en dehors de l’Union européenne dans le cadre des finalités ci-dessus mentionnées. 
+
+Lorsque le pays vers lequel vos données personnelles sont transmises ne présente pas un niveau de protection adéquat conformément à la règlementation, nous prenons les mesures appropriées pour nous assurer que les destinataires de vos données personnelles protègeront celles-ci de manière adéquate conformément à la présente Politique et à la réglementation applicable.
+9. Quels sont vos droits et comment pouvez-vous les exercer ?
+
+Conformément à la réglementation applicable en matière de protection des données à caractère personnel, vous pouvez :
+
+    Demander l’accès à vos données personnelles ;
+    Demander la rectification ou la suppression de vos données personnelles ;
+    Vous opposer à notre utilisation et au traitement de vos données personnelles ;
+    Nous demander de limiter l’utilisation et le traitement de vos données personnelles ;
+    Retirer votre consentement lorsque le traitement repose sur cette base ;
+    Demander la portabilité de vos données personnelles.
+
+Enfin, vous pouvez nous faire part de vos directives quant à la conservation, l’effacement et la communication de vos données personnelles dans l’hypothèse où vous décéderiez.
+
+Si vous souhaitez nous contacter au sujet de l’un de ces droits, vous pouvez nous joindre par courrier à :
+
+YOKO GANG
+15 rue Lourmel
+75015 PARIS
+
+Ou par courriel à l’adresse suivante : hello@Dibartolomeoshop.fr
+
+Nous vous adresserons une réponse dans un délai de 1 mois après la réception de votre demande.
+
+Dans un souci de confidentialité et de protection des données personnelles, une copie d’un titre d’identité signé pourra vous être demandée.
+
+En cas de réponse insatisfaisante de notre part, vous pouvez introduire une réclamation auprès de la CNIL. Nous vous invitons néanmoins à nous contacter aux coordonnées ci-dessus avant d’introduire toute réclamation auprès de la CNIL.
+</div>
+</template>
