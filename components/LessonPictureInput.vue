33e3ba5ccc2b5e80074f670ee8cb64c9cb2c55de
MatthiasChometon <matthiaschometon787@gmail.com>
2022-04-26 21:21:52 +0100

    feat: add LessonPictureInput component

---
+++ components/LessonPictureInput.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <v-file-input :rules="rules" accept="image/png, image/jpeg, image/bmp" placeholder="Choisissez l'image de votre cours"
+            prepend-icon="mdi-camera" label="L'image de mon cours" @change="onFileSelected">
+        </v-file-input>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            pictureDatas: undefined,
+            rules: [
+                value => !value || value.size < 20000000 || "La taille de l'image devrait etre inférieur à 20MB",
+            ]
+        }
+    },
+    methods: {
+        onFileSelected(file) {
+            this.pictureDatas = file
+        },
+    }
+}
+</script>
