c5f0662c77671a4ddd69442f4efa73daf4eacc10
MatthiasChometon <matthiaschometon787@gmail.com>
2022-04-30 00:31:36 +0100

    feat: update value on click

--- components/LessonPictureInput.vue
+++ components/LessonPictureInput.vue
@@ -9,20 +9,43 @@
 </template>
 
 <script>
+import { generateRandomId } from '../services/firestoreHelper'
+
 export default {
+    props: {
+        pictureDatas: {
+            required: true,
+            type: String,
+        },
+    },
     data() {
         return {
-            pictureDatas: undefined,
+            pictureSelected: undefined,
+            newPicture: undefined,
             rules: [
                 value => !value || value.size < 20000000 || "La taille de l'image devrait etre inférieur à 20MB",
             ]
         }
     },
-    methods: {
-        onFileSelected(file) {
-            this.pictureDatas = file
+    computed: {
+        getInitialPictureStore() {
+            return this.$store.getters.getStateFromString(this.$props.pictureDatas)
         },
-    }
+        pictureToDisplay() {
+            const hasSetNewPicture = this.newPicture
+            return hasSetNewPicture ? URL.createObjectURL(this.newPicture) : this.getInitialPictureStore.value
+        }
+    },
+    methods: {
+        onFileSelected(picture) {
+            this.pictureToDisplay = URL.createObjectURL(picture)
+            this.$store.commit('picture/set', { picture, stateName: this.getInitialPictureStore.fieldName })
+            this.$store.commit(`lesson/modify`, {
+                payload: { payload: { ...this.$store.state.lesson.form.payload, coverPicture: generateRandomId() } },
+                stateName: 'form',
+            })
+        },
+    },
 }
 </script>
 
