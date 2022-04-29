3821bed248e56bca620481b106248789470457fb
MatthiasChometon <matthiaschometon787@gmail.com>
2022-04-30 00:29:47 +0100

    feat: add preview picture

--- components/LessonPictureInput.vue
+++ components/LessonPictureInput.vue
@@ -3,6 +3,7 @@
         <v-file-input :rules="rules" accept="image/png, image/jpeg, image/bmp" placeholder="Choisissez l'image de votre cours"
             prepend-icon="mdi-camera" label="L'image de mon cours" @change="onFileSelected">
         </v-file-input>
+        <v-img max-height="100" max-width="100" :src="pictureToDisplay"></v-img>
     </div>
 </template>
 
