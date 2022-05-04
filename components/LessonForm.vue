98e0be7b609c8f6db103763c05a73a2195f9155d
MatthiasChometon <matthiaschometon787@gmail.com>
2022-05-04 09:37:49 +0100

    feat: use LessonDetailsIntroduction

--- components/LessonForm.vue
+++ components/LessonForm.vue
@@ -2,35 +2,7 @@
   <div>
     <v-form v-model="valid">
       <v-row class="justify-space-between ma-5">
-        <v-col class="pa-0">
-          <v-row class="ma-0 align-center d-flex">
-            <TextField
-              :get="`lesson.${$props.datas}.title`"
-              :rules="[(v) => !!v || 'Le titre est obligatoire']"
-              title
-              :open="open"
-              placeholder="Entrez le titre"
-            />
-          </v-row>
-          <v-row class="ma-0">
-            <p class="ma-0 align-center d-flex">
-              {{ lesson.studentIds ? lesson.studentIds.length : 0 }}/
-            </p>
-            <TextField
-              :suffix="$props.datas === 'new' ? '' : 'élèves'"
-              :get="`lesson.${$props.datas}.maximumStudents`"
-              :rules="[
-                (v) => !!v || 'Le nombre maximum d\'élèves est obligatoire',
-                (v) =>
-                  !isNaN(Number(v)) ||
-                  'Le nombre maximum d\'élèves doit etre un nombre',
-              ]"
-              number
-              :open="open"
-              placeholder="Entrez le nb max d'élèves"
-            />
-          </v-row>
-        </v-col>
+        <LessonDetailsIntroduction :state-name="$props.datas" />
         <div class="d-flex">
           <v-btn
             v-if="$props.datas !== 'new'"
