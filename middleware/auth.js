977fb71da78bf14b76b4cbdde99bd4bb69596b7c
MatthiasChometon <matthiaschometon787@gmail.com>
2022-05-12 22:02:55 +0100

    feat: check redirect page

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,12 +2,15 @@ export default function ({ store, redirect, route }) {
 
     const pageType = route.fullPath.split("/")[1]
     const userType = store.state.user.connected.type
+    const isVisitor = !store.state.user.connected.id
+    const isNotOnLoginPage = route.path !== '/authentification/login'
+    const isNotOnAuthorizedPage = userType !== pageType
 
-    if (!store.state.user.connected.id) {
+    if (isVisitor && isNotOnLoginPage) {
         return redirect('/authentification/login')
     }
 
-    if (userType !== pageType) {
+    if (!isVisitor && isNotOnAuthorizedPage) {
         return redirect(`/${userType}/lesson/planning`)
     }
 }
