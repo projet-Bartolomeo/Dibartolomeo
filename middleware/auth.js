a3ee65a3c1c13b99252c5364e972bfde2374084d
MatthiasChometon <matthiaschometon787@gmail.com>
2022-05-13 19:45:34 +0100

    feat: isNotOnAuthorizedPage const

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,13 +4,8 @@ export default function ({ store, redirect, route }) {
     const userType = store.state.user.connected.type
     const isVisitor = !store.state.user.connected.id
     const isNotOnLoginPage = route.path !== '/authentification/login'
-    const isNotOnAuthorizedPage = userType !== pageType
+    const isNotOnAuthorizedPage = (pageType === 'professor' && userType === 'student') || (pageType === 'student' && userType === 'professor')
 
-    if (isVisitor && isNotOnLoginPage) {
-        return redirect('/authentification/login')
-    }
-
-    if (!isVisitor && isNotOnAuthorizedPage) {
-        return redirect(`/${userType}/lesson/planning`)
-    }
+    if (isVisitor && isNotOnLoginPage) return redirect('/authentification/login')
+    if (isNotOnAuthorizedPage) return redirect(`/${userType}/lesson/planning`)
 }
